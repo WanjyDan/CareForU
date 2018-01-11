fe8796ee492099f6881f38f8fa421a58bdaa75ef
Dannie <Dannie@DESKTOP-PTQO0TI>
2018-01-11 09:55:19 +0300

    first commit

---
+++ app/src/main/java/com/cbitss/careforu/view/FullScreenImageActivity.java
@@ -0,0 +1,14 @@
+package com.cbitss.careforu.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.cbitss.careforu.R;
+
+public class FullScreenImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_image);
+    }
+}
---
+++ app/src/main/java/com/cbitss/careforu/view/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.cbitss.careforu.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.cbitss.careforu.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
